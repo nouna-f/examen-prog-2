e80f9b3f1c01aae2cc5e7c00d02f306a13fc2843
nouna-f <nourine.founouni@udla.edu.ec>
2023-06-06 13:01:11 -0500

    sumador notas

--- Prog2.c
+++ Prog2.c
@@ -11,3 +11,15 @@ int calcularPromedioAlumnos(int calificaciones[3]) {
     }
     return suma / 3;
 }
+void calcularPromedioGrupo(int calificaciones[23][3], int promediosGrupo[3]) {
+    int i, j;
+    int suma;
+    for (j = 0; j < 3; j++) {
+        suma = 0;
+        for (i = 0; i < 23; i++) {
+            suma += calificaciones[i][j];
+        }
+        ///toda la suma de los promedios la dividimos para el numero de estudiantes 
+        promediosGrupo[j] = suma / 23;
+    }
+}
